b458c420b83c14af5f02d90edf48d797fe5a6b0b
Vincent Chang <vincentchang402@gmail.com>
2024-02-03 00:04:45 +0800

    Big submit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h2 className="text-blue-600 font-extrabold text-5xl">Tailwind CSS Is Working!</h2>
     </div>
-  );
+  )
 }
