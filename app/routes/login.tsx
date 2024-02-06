fba520a398e6f2c02750cd176220f39ef7775aa6
Vincent Chang <vincentchang402@gmail.com>
2024-02-06 15:50:24 +0800

    Add error field (and fix typo)

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -71,26 +71,42 @@ export const action: ActionFunction = async ({ request }) => {
   }
 };
 export default function Login() {
-  const [action, setAction] = useState("login");
-  const [formData, setFormDate] = useState({
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
+  const actionData = useActionData();
+  const [formError, setFormError] = useState(actionData?.error || "");
+  const [errors, setErrors] = useState(actionData?.errors || {});
+  const [action, setAction] = useState(actionData?.form || "login");
+  const [formData, setFormData] = useState({
+    email: actionData?.fields?.email || "",
+    password: actionData?.fields?.password || "",
+    firstName: actionData?.fields?.firstName || "",
+    lastName: actionData?.fields?.lastName || "",
   });
+
+  const handleActionChange = () => {
+    const newState = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+    };
+    setErrors(newState);
+    setFormError("");
+    setFormData(newState);
+    setAction(action == "login" ? "register" : "login");
+  };
 
   const handleInputChange = (
     event: React.ChangeEvent<HTMLInputElement>,
     field: string
   ) => {
-    setFormDate((form) => ({ ...form, [field]: event.target.value }));
+    setFormData((form) => ({ ...form, [field]: event.target.value }));
   };
 
   return (
     <Layout>
       <div className="h-full justify-center items-center flex flex-col gap-y-4">
         <button
-          onClick={() => setAction(action == "login" ? "register" : "login")}
+          onClick={handleActionChange}
           className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
         >
           {action === "login" ? "Sign Up" : "Sign In"}
@@ -105,17 +121,22 @@ export default function Login() {
         </p>
 
         <form method="post" className="rounded-2xl bg-gray-200 p-6 w-96">
+          <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+            {formError}
+          </div>
           <FormField
             htmlFor="email"
             label="Email"
             value={formData.email}
             onChange={(e) => handleInputChange(e, "email")}
+            error={errors?.email}
           />
           <FormField
             htmlFor="password"
             label="Password"
             value={formData.password}
             onChange={(e) => handleInputChange(e, "password")}
+            error={errors?.password}
           />
           {action === "register" && (
             <>
@@ -124,12 +145,14 @@ export default function Login() {
                 label="First Name"
                 onChange={(e) => handleInputChange(e, "firstName")}
                 value={formData.firstName}
+                error={errors?.firstName}
               />
               <FormField
                 htmlFor="lastName"
                 label="Last Name"
                 onChange={(e) => handleInputChange(e, "lastName")}
                 value={formData.lastName}
+                error={errors?.lastName}
               />
             </>
           )}
